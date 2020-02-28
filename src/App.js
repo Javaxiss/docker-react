f71555a1fe81f8361c847271fe5008d890bc39ac
issam <issam.boualem@prest-interiale.fr>
2020-02-28 10:28:10 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I was changed
         </p>
-        <p>Bye there !</p>
+       
         <a
           className="App-link"
           href="https://reactjs.org"
